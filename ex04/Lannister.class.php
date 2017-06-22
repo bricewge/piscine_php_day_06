a71d48a6ba37e416ed76172af10e91fcc7710864
Brice Waegeneire <brice.wge@gmail.com>
2017-06-22 16:31:11 +0200

    ex04 more straigth foreward logic and DRY

--- ex04/Lannister.class.php
+++ ex04/Lannister.class.php
@@ -2,10 +2,10 @@
 
 class Lannister {
     public function sleepWith($person) {
-        if (get_parent_class($person) != "Lannister")
-            echo "Let's do this.\n";
-        else
+        if (get_parent_class($person) == get_parent_class($this))
             echo "Not even if I'm drunk !\n";
+        else
+            echo "Let's do this.\n";
     }
 }
 
